df261e5d5c0e802ebb34f0acca7396079ce060d3
k-hayack884 <hayac885@gmail.com>
2022-01-14 06:57:31 +0000

    マルチログイン

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -44,8 +44,13 @@ class LoginRequest extends FormRequest
     public function authenticate()
     {
         $this->ensureIsNotRateLimited();
-
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if ($this->routeIs('admin.*')) {
+            $guard='admin';
+        } else {
+            $guard='users';
+        }
+        
+        if (! Auth::guard($guard)->attempt($this->only('email', 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([

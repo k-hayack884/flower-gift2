f86971229a59ac90de3b6d5078f360efce0bb93c
k-hayack884 <hayack885@gmail.com>
2022-01-19 13:56:56 +0900

    bladeを成型

--- resources/views/admin/expired-users.blade.php
+++ resources/views/admin/expired-users.blade.php
@@ -1,70 +1,79 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-           退会済みユーザー管理
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      退会済みユーザー管理
+    </h2>
+  </x-slot>
 
-    <div clas済み
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <section class="text-gray-600 body-font">
-  <div class="container px-5 mx-auto">
+  <div>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 bg-white border-b border-gray-200">
+          <section class="text-gray-600 body-font">
+            <div class="container px-5 mx-auto">
 
- <x-flash-message status="session('status')"></x-flash-message>
-        <div class="flex justify-between w-full mb-4">
-      <h1 class="sm:text-4xl font-medium title-font text-gray-900  ">退会済みユーザー管理</h1>
-    </div>
+              <x-flash-message status="session('status')"></x-flash-message>
+              <div class="flex justify-between w-full mb-4">
+                <h1 class="sm:text-4xl font-medium title-font text-gray-900  ">退会済みユーザー管理</h1>
+              </div>
 
 
-    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
-      <table class="table-auto w-full text-left whitespace-no-wrap">
-        <thead>
-          <tr>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">ユーザー名</th>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">前回のログイン</th>
-            <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
-          </tr>
-        </thead>
-        <tbody>
-            @foreach($expireUsers as $expireUser)
-          <tr>
-            <td class="px-4 py-3">{{$expireUser->name}}</td>
-            <td class="px-4 py-3">{{$expireUser->email}}</td>
-            <td class="px-4 py-3">
-                @if($expireUser->deleted_at->diffInDays(now())>=30)
-                {{$expireUser->deleted_at->subDays(30)}}
-                @else
-                {{$expireUser->deleted_at->diffForHumans()}}
-                @endif
-                </td>
-            <td class="w-10 text-center">
-             <form id="delete_{{ $expireUser->id }}" method="post" action="{{ route('admin.expired-users.destroy',['user'=>$expireUser->id]) }}">
-            @csrf
-          <a href="#" data-id="{{$expireUser->id}}" onclick="deletePost(this)" class="flex mx-auto  text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg mx-4"> 完全に削除</a>
-            </form>
-            </td>
-          </tr>
-          @endforeach
+              <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+                <table class="table-auto w-full text-left whitespace-no-wrap">
+                  <thead>
+                    <tr>
+                      <th
+                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                        ユーザー名</th>
+                      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                        メールアドレス</th>
+                      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                        前回のログイン</th>
+                      <th
+                        class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($expireUsers as $expireUser)
+                    <tr>
+                      <td class="px-4 py-3">{{$expireUser->name}}</td>
+                      <td class="px-4 py-3">{{$expireUser->email}}</td>
+                      <td class="px-4 py-3">
+                        @if($expireUser->deleted_at->diffInDays(now())>=30)
+                        {{$expireUser->deleted_at->subDays(30)}}
+                        @else
+                        {{$expireUser->deleted_at->diffForHumans()}}
+                        @endif
+                      </td>
+                      <td class="w-10 text-center">
+                        <form id="delete_{{ $expireUser->id }}" method="post"
+                          action="{{ route('admin.expired-users.destroy',['user'=>$expireUser->id]) }}">
+                          @csrf
+                          <a href="#" data-id="{{$expireUser->id}}" onclick="deletePost(this)"
+                            class="flex mx-auto  text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg mx-4">
+                            完全に削除</a>
+                        </form>
+                      </td>
+                    </tr>
+                    @endforeach
 
-        </tbody>
-      </table>
-    </div>
-</section>
-                </div>
-            </div>
+                  </tbody>
+                </table>
+              </div>
+          </section>
         </div>
+      </div>
     </div>
-    <script src="{{ mix('/js/app.js') }}"></script>
-        <script>
-        'use strict';
+  </div>
+  <script src="{{ mix('/js/app.js') }}"></script>
+  <script>
+    'use strict';
         function deletePost(e){
         if(confirm('本当に削除してもよろしいですか？')){
           document.getElementById('delete_'+e.dataset.id).submit();
         }
         }
-        </script>
+  </script>
 
 </x-app-layout>
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -24,46 +24,54 @@
                                             <div class="relative">
                                                 <label for="name" class="leading-7 text-sm text-gray-600">ユーザー名</label>
                                                 <input type="text" id="name" name="name"
-                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" value="{{ old('name') }}" required>
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                                    value="{{ old('name') }}" required>
                                             </div>
                                         </div>
                                         <div class="p-2 w-1/2 mx-auto ">
                                             <div class="relative">
-                                                <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
+                                                <label for="email"
+                                                    class="leading-7 text-sm text-gray-600">メールアドレス</label>
                                                 <input type="email" id="email" name="email"
-                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" value="{{ old('email') }}" required>
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                                    value="{{ old('email') }}" required>
                                             </div>
                                         </div>
                                         <div class="p-2 w-1/2 mx-auto">
                                             <div class="relative">
-                                                <label for="password" class="leading-7 text-sm text-gray-600">パスワード</label>
+                                                <label for="password"
+                                                    class="leading-7 text-sm text-gray-600">パスワード</label>
                                                 <input type="password" id="password" name="password"
-                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                                    required>
                                             </div>
                                         </div>
                                         <div class="p-2 w-1/2 mx-auto">
                                             <div class="relative">
-                                                <label for="password_confirmation" class="leading-7 text-sm text-gray-600">パスワード確認</label>
-                                                <input type="password" id="password_confirmation" name="password_confirmation"
-                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
+                                                <label for="password_confirmation"
+                                                    class="leading-7 text-sm text-gray-600">パスワード確認</label>
+                                                <input type="password" id="password_confirmation"
+                                                    name="password_confirmation"
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                                    required>
                                             </div>
                                         </div>
-                                                                          </div>
-                                     <div class="flex justify-around mt-4">
-    
-                                                <button type="button" onclick="location.href='{{ route('admin.users.index') }}'"
-                                                    class="flex mx-auto  text-white bg-gray-500 border-0 py-2 px-12 focus:outline-none hover:bg-gray-600 rounded text-lg mx-4">戻る</button>
-                                                <button type="submit"
-                                                    class="flex mx-auto  text-white bg-indigo-500 border-0 py-2 px-12 focus:outline-none hover:bg-indigo-600 rounded text-lg mx-4">編集</button>
-                                                    </div>
-</form>
-                                </div>
+                                    </div>
+                                    <div class="flex justify-around mt-4">
+
+                                        <button type="button" onclick="location.href='{{ route('admin.users.index') }}'"
+                                            class="flex mx-auto  text-white bg-gray-500 border-0 py-2 px-12 focus:outline-none hover:bg-gray-600 rounded text-lg mx-4">戻る</button>
+                                        <button type="submit"
+                                            class="flex mx-auto  text-white bg-indigo-500 border-0 py-2 px-12 focus:outline-none hover:bg-indigo-600 rounded text-lg mx-4">編集</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-                    </section>
-
                 </div>
+                </section>
+
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>

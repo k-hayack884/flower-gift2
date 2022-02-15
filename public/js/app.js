784342d2f17891c93acf8fc823ccfd57f847e87d
k-hayack884 <hayack885@gmail.com>
2022-02-15 17:49:20 +0900

    アプデ

--- public/js/app.js
+++ public/js/app.js
@@ -25229,7 +25229,7 @@ alpinejs__WEBPACK_IMPORTED_MODULE_6__["default"].start();
 window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
+ * to our Larav back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 

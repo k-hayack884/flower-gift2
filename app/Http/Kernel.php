2bdfae8ce28538e21419eb25e54446003751c629
k-hayack884 <hayack885@gmail.com>
2022-03-08 18:32:31 +0900

    Sanctumのテスト

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class, //401の正体
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],

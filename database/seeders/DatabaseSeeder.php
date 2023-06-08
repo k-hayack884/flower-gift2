c0d8606190b7ceb884595f259ee8d644df385edb
k-hayack884 <hayack885@gmail.com>
2023-06-08 15:57:38 +0900

    修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class,
+//            UserSeeder::class,
             AdminSeeder::class,
             CategorySeeder::class,
             // ProductSeeder::class,
